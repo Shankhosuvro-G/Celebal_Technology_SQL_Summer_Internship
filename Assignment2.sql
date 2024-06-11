949e748d4833d5713e2eeb4e890b60f8ba76db4b
Shankhosuvro Ghosh <98182979+Shankhosuvro-G@users.noreply.github.com>
2024-06-11 19:58:44 +0530

    Update Assignment2.sql

--- Assignment2.sql
+++ Assignment2.sql
@@ -70,6 +70,33 @@ if @@ROWCOUNT=0
 raiserror('Not Found',16,1)
 end
 
+-- Create a procedure GetOrderDetails that takes OrderID as input parameter and returns all the records for that OrderID. If no records are found in Order Details table, then it should
+-- print the line: "The OrderID XXXX does not exits", where XXX should be the OrderlD entered by user and the procedure should RETURN the value 1.
 
+create procedure GetOrderDetails
+@OrderID int
+as
+begin
+if not exists (select 1 from SalesLT.SalesOrderDetail where SalesOrderID=@OrderID)
+print 'The OrderID'+ convert(varchar(10),@OrderID)+'does not exist'
+else
+select * from SalesLT.SalesOrderDetail where SalesOrderID=@OrderID
+return 1
+end
+
+-- Create a procedure DeleteOrderDetails that takes OrderID and ProductID and deletes that from Order Details table. Your procedure should validate parameters. It should retum an error
+-- code (-1) and print a message if the parameters are invalid. Parameters are valid if the given order ID appears in the table and if the given product ID appears in that order.
+
+create procedure DeleteOrderDetails
+@OrderID int,
+@ProductID int
+as
+begin
+if not exists (select 1 from SalesLT.SalesOrderDetail where SalesOrderID=@OrderID and ProductID=@ProductID)\
+raiserror('Parameters are invalid',16,1)
+else
+delete from SalesLT.SalesOrderDetail where SalesOrderID=@OrderID and ProductID=@ProductID
+return 0;
+end
 
 
